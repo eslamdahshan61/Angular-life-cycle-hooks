cc5da7fb0c03d0949e2f4b9562792b01ec09a850
eslamdahshan <eslamdahshan61@gmail.com>
2020-05-13 02:43:54 +0200

    aa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'life-cycle-hooks';
+ private number:number= 2000;
+
+ isVisible:Boolean = true;
+ switch(){
+   this.isVisible = !this.isVisible;
+ }
+
+  name:string= "Eslam";
+user={
+  name:"Eslam"
+}
+
+
+  UpdateValue(){
+    this.user.name = "Eslam Dahshan"
+  }
+
+  get counter(){
+  return  this.number;
+  }
+
+
+set counter(value){
+   this.number =value;
+  }
+
+  Increment(){
+    this.counter++;
+  }
+
+  decrement(){
+    this.counter--;
+  }
+
+
+
+
 }
---
+++ src/app/child/child.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit, Input, OnChanges, SimpleChanges, SimpleChange, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css']
+})
+export class ChildComponent implements OnInit,OnChanges,DoCheck,AfterContentInit , AfterContentChecked ,AfterViewInit , AfterViewChecked , OnDestroy {
+ 
+  
+  private myNumber:number ;
+
+  @Input() myNewName:any ;
+  
+
+  constructor() { }
+  @Input() 
+  set myNewNumber(number:number){
+    this.myNumber = number ;
+  }
+  get myNewNumber(){
+   return this.myNumber ;
+  }
+
+  ngOnChanges(changes:SimpleChanges){
+    const newNumberChange:SimpleChange = changes.myNewNumber;
+    
+    console.log("Previous Value - (ngOnChanges)", newNumberChange.previousValue);
+    console.log("Current Value - (ngOnChanges)", newNumberChange.currentValue);
+    this.myNewNumber = newNumberChange.currentValue;
+  }
+  ngOnInit(): void {
+    console.log("Value - (ngOnInit)", this.myNewNumber);
+  }
+  ngDoCheck(){
+    console.log("Value - (ngDoCheck)", this.myNewName , this.myNewNumber)
+  }
+  ngAfterContentInit(){
+    console.log("Value - (ngAfterContentInit)", "Hi!!!!")
+  }
+  ngAfterContentChecked(){
+    console.log("Value executed - (ngAfterContentChecked)")
+    /* alert("hi"); */
+  }
+
+  ngAfterViewInit(){
+    console.log("Value executed - (ngAfterViewInit)")
+  }
+  ngAfterViewChecked(){
+    console.log("Value executed - (ngAfterViewChecked)")
+    alert("hi");
+  }
+  ngOnDestroy(){
+    console.log("Component has been destroyed - (ngOnDestroy)")
+  }
+  
+  
+}
